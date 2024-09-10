fef831fdf37e0d7921c3a4e5b9fdd8f9cc19de1e
x-77@live.com <x-77@live.com>
2024-09-10 22:33:12 +0300

    Working word bank, a lot remains to tweak :(

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
+
 const path = require('path');
 const fs = require('fs');
 const app = express();
+app.use(express.json()); // Parse JSON data
+app.use(express.urlencoded({ extended: false }))
 const port = 3000;
 
 //Checking if progress file exists!
 const sqlite3 = require('sqlite3').verbose();
-const dbFile = require('./db');
+const dbOps = require('./db');
 if (fs.existsSync('progress/database.db'));
-else dbFile.initProgress();  
+else dbOps.initProgress();  
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
@@ -16,6 +19,8 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+
 // Route to render the index.ejs
 const db = new sqlite3.Database('progress/database.db');
 app.get('/',  (req, res) => {
@@ -23,12 +28,19 @@ app.get('/',  (req, res) => {
      res.render('index', {wordsCount:rows[0].count});
   });
 });
+
+app.post("/processQuestion",  async (req, res) => {
+  await dbOps.dbCommand(db, `UPDATE words SET status=${req.body.score}, updated_at=${Date.now()} where id = ${req.body.id}`).then(()=>{
+    res.send("success!");
+  });
+});
+
 app.post("/getRandomQuestion", (req, res)=>{
   db.all('SELECT * FROM words where status is null limit 1', (err, row) => {
-    console.log(row);
     res.json(row[0]);
   });
 });
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,6 @@
 
 const getRandomQuestion = () => {
+    $("#QuestionId").val("");
     $.ajax({
         url: "getRandomQuestion",
         method: "POST",
@@ -7,9 +8,27 @@ const getRandomQuestion = () => {
             console.log(data);
             $('.word-b').html( data.word[0].toUpperCase() + data.word.slice(1));
             $('.word-d').html(data.definition);
+            $(".word-t").html(data.wordtype);
+            $("#QuestionId").val(data.id);
+            $(".translateGoogle").attr("href", "https://translate.google.com/?sl=en&text="+data.word+"&op=translate");
+            $(".showIt").fadeIn();
+            $('.word-d').hide();
         }
     });
 }
+const processQuestion = (score) => {
+    const id = $("#QuestionId").val();
+    $.ajax({
+      url: "/processQuestion",
+      method: "POST",
+      
+      data: { id: id, score: score },
+      success: function(data) {
+        getRandomQuestion();
+      }
+    });
+};
+
 $(".showIt").on('click', function(){
     $('.word-d').fadeIn();
     $(this).hide();
